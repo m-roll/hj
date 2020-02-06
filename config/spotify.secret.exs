73ad0a44d844497e6a41c8c0208274da5beaea57
Matt Rollender <mroll@mit.edu>
2020-02-06 17:11:33 -0500

    Fucked up gitignore oops

--- config/spotify.secret.exs
+++
@@ -1,6 +0,0 @@
-use Mix.Config
-
-config :spotify_ex,
-  user_id: "<YOUR SPOTIFY USER ID>",
-  scopes: ["streaming", "user-read-playback-state", "user-modify-playback-state"],
-  callback_url: "http://localhost:4000/auth"
