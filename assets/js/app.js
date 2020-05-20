eaf9ddc4caf320f2f862c83e7dd04880ed470e5f
Matt Rollender <mroll@mit.edu>
2020-05-20 15:38:35 -0400

    Add font awesome icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,6 @@ import '../scss/bootstrap-custom.scss';
 import '@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css';
 import '@forevolve/bootstrap-dark/dist/js/bootstrap.min.js';
 import "../scss/app.scss";
+import "font-awesome/scss/_icons.scss";
 import JukeboxController from "./controller/jukebox";
 let controller = new JukeboxController();
