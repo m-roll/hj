a750618e019ee3c04667b93d34426aa4fc903909
Matt Rollender <mroll@mit.edu>
2020-05-20 17:23:31 -0400

    Add button for devies -- no functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,14 @@ import '../scss/bootstrap-custom.scss';
 import '@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css';
 import '@forevolve/bootstrap-dark/dist/js/bootstrap.min.js';
 import "../scss/app.scss";
-import "font-awesome/scss/_icons.scss";
+import {
+  library,
+  dom
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faHeadphones
+} from '@fortawesome/free-solid-svg-icons'
 import JukeboxController from "./controller/jukebox";
+library.add(faHeadphones);
+dom.watch();
 let controller = new JukeboxController();
